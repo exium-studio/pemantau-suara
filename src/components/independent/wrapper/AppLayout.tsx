35315ce74cf79684ef2507355b3aca5063c6d6d6
sulenq <whyimustwriteit@gmail.com>
2024-09-21 08:09:57 +0700

    styling

--- src/components/independent/wrapper/AppLayout.tsx
+++ src/components/independent/wrapper/AppLayout.tsx
@@ -69,7 +69,7 @@ export default function AppLayout({ children, activeIndex }: AppLayoutProps) {
       </VStack>
 
       <CContainer justify={"center"} align={"center"}>
-        <MapboxMap latitude={-7.05} longitude={110.38} zoom={11} />
+        <MapboxMap latitude={-7.02} longitude={110.38} zoom={11} />
       </CContainer>
 
       {/* Detail GeoJSON Data */}

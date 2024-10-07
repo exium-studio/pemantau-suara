257ce4c940c13b8ae11dc86fa74143cba249e003
sulenq <whyimustwriteit@gmail.com>
2024-10-07 09:45:42 +0700

    foto_aktivitas  validation

--- src/components/form/ActivityForm.tsx
+++ src/components/form/ActivityForm.tsx
@@ -103,6 +103,8 @@ export default function ActivityForm({
               ".PNG",
               ".jpg",
               ".JPG",
+              ".jpeg",
+              ".JPEG",
               ".heic",
               ".HEIC",
             ];

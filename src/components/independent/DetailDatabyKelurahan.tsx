28da1b71abadc60c18e5ce330d7d017a85cab2d0
sulenq <whyimustwriteit@gmail.com>
2024-10-19 13:03:40 +0700

    styling

--- src/components/independent/DetailDatabyKelurahan.tsx
+++ src/components/independent/DetailDatabyKelurahan.tsx
@@ -153,7 +153,7 @@ const AktivitasChart = ({ data, data2 }: any) => {
         </Text>
 
         <VStack flex={"1 0 0"} position={"relative"}>
-          <VStack w={"100%"} className="doughnutChartContainer">
+          <VStack zIndex={2} w={"100%"} className="doughnutChartContainer">
             <ChartDoughnut labels={labels} datasets={datasets} cutout={"70"} />
           </VStack>
 
